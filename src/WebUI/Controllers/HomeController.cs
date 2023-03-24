4465968f46f2fef2feb004788d7d3fd426e99bf3
enesucar <enesucar1414@gmail.com>
2023-03-25 00:08:38 +0300

    Add project files.

---
+++ src/WebUI/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet("team")]
+        public IActionResult Team()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
